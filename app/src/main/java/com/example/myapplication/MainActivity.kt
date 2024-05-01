11677741e24cf5cbac96024d17bad4b1b36f7745
KRUSHANG <krushangparekh999@gmail.com>
2024-05-01 11:45:33 +0530

    four commit -> toaster

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.mytoasty.Toaster
 
 class MainActivity : AppCompatActivity() {
